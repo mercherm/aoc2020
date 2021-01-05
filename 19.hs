b67f8c6b0a9e3af80798c363cd6e877bc3b5d486
Herm <mercherm44@gmail.com>
2021-01-05 11:08:26 -0700

    day 19 done

--- 19.hs
+++ 19.hs
@@ -67,29 +67,38 @@ process m id = foo id where
     Just x -> concatMap (\l -> foldl (\b i -> cProd (++) b (foo i)) [[]] l ) x
     Nothing -> [[k]]
 
-process2 m mx = foo 0 0 where
-  foo k d = if d == mx then [[k]] else case M.lookup k m of
-    Just x -> concatMap (\l -> foldl (\b i -> cProd (++) b (foo i (d+1))) [[]] l ) x
-    Nothing -> [[k]]
+every f xs = length xs == length (filter f xs)
+
+--  8: 42 | 8
+-- 11: 42 31 | 42 11 31
+--  0: 8 11
+--  0: [42,...,42] [42,...,42,31,...,31]
+matchesZero fortyTwo thirtyOne str = alright where
+  len = length $ fortyTwo!!0 
+  chunks = S.chunksOf len str
+  (left, right) = span (`elem` fortyTwo) chunks
+  lenL = length left
+  lenR = length right
+  alright = lenL >= 2 && lenR > 0 && lenL > lenR && every (`elem` thirtyOne) right
 
 solution inp = do
   let [rs, ms] = S.splitOn "\n\n" inp
   case parse rulesPsr "" rs of
     Right x -> do
+      let msgs = lines ms
       let chars = M.fromList $ fSnds getCharVal (filter ((isCharRule).snd) x)
       let subs = M.fromList $ fSnds getSubVal (filter ((isSubRule).snd) x)
-      let strs = map (map (chars M.!)) (process subs 0)
-      let msgs = lines ms
-      let mx = maximum (map length msgs)
-      let matches = intersect msgs strs
-      print $ length matches
-      let p2Subs = M.delete 8 (M.delete 11 subs)
-      let p2Chars = M.insert 8 'c' (M.insert 11 'd' chars)
-      -- I don't expect this to work. 
-      let p2Strs = map (map (p2Chars M.!)) (process2 p2Subs mx)
-      print p2Strs
-      --let p2Matches = intersect msgs p2Strs
-      --print $ length p2Matches
+      -- this is my (far too slow) solution to part one:
+      --let strs = map (map (chars M.!)) (process subs 0)
+      --let matches = intersect msgs strs
+      --print $ length matches
+      let fortyTwo = map (map (chars M.!)) (process subs 42)
+      let thirtyOne = map (map (chars M.!)) (process subs 31)
+      let matches2 = filter (matchesZero fortyTwo thirtyOne) msgs
+      -- debug messages for posterity:
+      --print $ map (span (`elem` fortyTwo)) (map (S.chunksOf 8) matches2)
+      --putStr $ unlines matches2
+      print $ length $ matches2
 
 main = do
   inp <- getContents
